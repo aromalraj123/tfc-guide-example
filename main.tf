7a8490cef58e0f48e016ba574d3a5a334b0d116b
aromalraj123 <39479225+aromalraj123@users.noreply.github.com>
2022-05-30 21:04:27 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -18,7 +18,7 @@ resource "aws_instance" "web" {
   ami           = data.aws_ami.ubuntu.id
   instance_type = "t3.micro"
 
-  tags = {
-    Name = "HelloWorld"
-  }
+#  tags = {
+#    Name = "HelloWorld"
+#  }
 }
