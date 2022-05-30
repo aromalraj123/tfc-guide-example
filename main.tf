bcaf57c73d6468ab54db89bfc0713fd7797aefd5
aromalraj123 <39479225+aromalraj123@users.noreply.github.com>
2022-05-30 21:14:45 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,3 +1,15 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 3.28.0"
+    }
+  }
+  required_version = ">= 0.14.0"
+}
+provider "aws" {
+  region = "us-east-1"
+}
 data "aws_ami" "ubuntu" {
   most_recent = true
 
@@ -17,7 +29,6 @@ data "aws_ami" "ubuntu" {
 resource "aws_instance" "web" {
   ami           = data.aws_ami.ubuntu.id
   instance_type = "t3.micro"
-
 #  tags = {
 #    Name = "HelloWorld"
 #  }
